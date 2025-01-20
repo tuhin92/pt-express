ff521e295bc15ba9a00998bafa1ff884ea4ab809
tuhin92 <tuhinalmamun71m@gmail.com>
2025-01-20 23:56:31 +0600

    Installing express, typescript

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { Server } from "http";
+import app from "./app";
+const PORT = 5000;
+
+let server: Server;
+async function startServer() {
+  server = app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+  });
+}
+
+startServer();
