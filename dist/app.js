2f54304b6bdfdb8aebf9a94b761199e128235e77
tuhin92 <tuhinalmamun71m@gmail.com>
2025-01-21 01:15:53 +0600

    Routing in express.js

--- dist/app.js
+++ dist/app.js
@@ -9,18 +9,41 @@ const port = 3000;
 //parse json
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+//router
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+//user routes
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User created successfully",
+    });
+});
+//course routes
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "Course created successfully",
+    });
+});
 //middleware
 const log = (req, res, next) => {
     console.log(req.url, req.method, req.body, req.hostname);
     next();
 };
-app.get('/', log, (req, res) => {
-    res.send('Server is running!');
+app.get("/", log, (req, res) => {
+    res.send("Server is running!");
 });
-app.post('/', log, (req, res) => {
+app.post("/", log, (req, res) => {
     console.log(req.body);
     res.json({
-        message: 'Data received',
+        message: "Data received",
     });
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,54 @@
-import express, {NextFunction, Request, Response} from 'express'
-const app = express()
-const port = 3000
+import express, { NextFunction, Request, Response } from "express";
+const app = express();
+const port = 3000;
 
 //parse json
 app.use(express.json());
 app.use(express.text());
 
+//router
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+//user routes
+userRouter.post("/create-user", (req: Request, res: Response) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User created successfully",
+    })
+});
+
+//course routes
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "Course created successfully",
+    })
+});
 
 //middleware
-const log = (req : Request, res: Response, next: NextFunction) => {
-    console.log(req.url, req.method, req.body, req.hostname);
-    next();
-}
+const log = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.body, req.hostname);
+  next();
+};
 
-app.get('/', log, (req: Request , res: Response) => {
-    res.send('Server is running!')
-})
+app.get("/", log, (req: Request, res: Response) => {
+  res.send("Server is running!");
+});
 
-app.post('/', log, (req: Request , res: Response) => {
-    console.log(req.body);
-    res.json({
-        message: 'Data received',
-    })
-})
+app.post("/", log, (req: Request, res: Response) => {
+  console.log(req.body);
+  res.json({
+    message: "Data received",
+  });
+});
 
 export default app;
